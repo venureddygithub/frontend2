c259dbb00ea19aa2f66f0aa2069a55ac4f23cae1
venureddygithub <144114409+venureddygithub@users.noreply.github.com>
2024-10-14 18:33:56 +0530

    Update NotFound.jsx

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,3 +1,13 @@
 export const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <>
+      <div className="cart-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+          alt="not-found"
+          className="not-found-img"
+        />
+      </div>
+    </>
+  );
 };
